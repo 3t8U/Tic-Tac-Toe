878a8860713e7c12f502e96d818239a0a583a47d
Student <example@example.com>
2019-11-06 13:52:54 -0800

    add checkWin functionality

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,50 @@ function Game() {
                 ["", "", ""],
                 ["", "", ""]];
   this.players = "humans";
-  this.currentTurn = ((Math.random() > 0.5) ? "alien" : "human");
+  this.currentPlayer = ((Math.random() > 0.5) ? "alien" : "human");
+  this.turnNumber = 0;
 }
 
-Game.prototype.playerMove = function(spot) {
-  this.board[spot[0]][spot[1]] = this.currentTurn;
-  this.currentTurn = ((this.currentTurn === "alien") ? "human" : "alien");
-  // return true;
 
-  var winCheck = {}
-  for (var r=0; r<3; r++) {
-    winCheck = {};
-    for (var c=0; c<3; c++) {
-      winCheck[this.board[r][c]]++;
-    }
-    if (winCheck.values().includes(3)) {
-      alert("You win!");
-    }
+Game.prototype.playerMove = function(spot, id) {
+  this.turnNumber++;
+  this.board[spot[0]][spot[1]] = this.currentPlayer;
+  if (this.checkWin(spot, id)) {
+    alert("YOU WIN");
+  } else {
+    this.currentPlayer = ((this.currentPlayer === "alien") ? "human" : "alien");
+    return false;
   }
 }
+
+Game.prototype.checkWin = function(spot, id) {
+  if (this.turnNumber <5){
+    return false;
+  }
+  var rowCheck = 0;
+  var colCheck = 0;
+  for(var i = 0; i < 3; i++) {
+    rowCheck += ((this.board[spot[0]][i] === this.currentPlayer) ? 1 : 0);
+    colCheck += ((this.board[i][spot[1]] === this.currentPlayer) ? 1 : 0);
+  }
+
+  if ((rowCheck === 3) || colCheck === 3) return true;
+
+  var diaCheck = 0;
+    diaCheck += ((this.board[0][0] === this.currentPlayer) ? 1 : 0);
+    diaCheck += ((this.board[1][1] === this.currentPlayer) ? 1 : 0);
+    diaCheck += ((this.board[2][2] === this.currentPlayer) ? 1 : 0);
+  if (diaCheck === 3) return true;
+
+  diaCheck = 0;
+    diaCheck += ((this.board[0][2] === this.currentPlayer) ? 1 : 0);
+    diaCheck += ((this.board[1][1] === this.currentPlayer) ? 1 : 0);
+    diaCheck += ((this.board[2][0] === this.currentPlayer) ? 1 : 0);
+  if (diaCheck === 3) return true;
+
+  return false;
+}
+
 
 
  //UI Logic
@@ -39,8 +64,8 @@ $(document).ready(function(){
     var spot = this.id.split("");
 
     if ( !(freshGame.board[spot[0]][spot[1]]) ) {
-      $(this).children().attr("src", "img/" + freshGame.currentTurn + ".png");
-      freshGame.playerMove(spot);
+      $(this).children().attr("src", "img/" + freshGame.currentPlayer + ".png");
+      freshGame.playerMove(spot, this.id);
     } else {
       alert("Cheaters never win.");
     }
