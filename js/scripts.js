44e5d6bd90c51fafb5df9e46e924a2d2e992c5cd
Student <example@example.com>
2019-11-07 10:31:08 -0800

    clean up checkWin function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 // Business End
+
+   // Business Logic for Game Board//
 function Game() {
   this.board = [["", "", ""],
                 ["", "", ""],
@@ -6,18 +8,17 @@ function Game() {
   this.players = "humans";
   this.currentPlayer = ((Math.random() > 0.5) ? "alien" : "human"); //Assigns Random Starting Player
   this.turnNumber = 0;
+  this.cheatR = {human: 0, alien: 0};
 }
 
 Game.prototype.playerMove = function(spot) {
   this.turnNumber++;
   this.board[spot[0]][spot[1]] = this.currentPlayer;     // Assigns board Value
   var result = this.checkWin(spot);
-  if (this.checkWin(spot)) {
-    return result;
-  } else {
+  if (!result) {
     this.currentPlayer = ((this.currentPlayer === "alien") ? "human" : "alien");  // Switches Player
-    return result;
   }
+  return result;
 }
 
 Game.prototype.spotCheck = function(row, col) {
@@ -25,17 +26,15 @@ Game.prototype.spotCheck = function(row, col) {
 }
 
 Game.prototype.checkWin = function(spot) {
-  if (this.turnNumber < 5){
-    return false; // Early Game Return
-  }
+  if (this.turnNumber < 5) return false; // Early Game Return
 
   var winCheck = [0, 0, 0, 0]; // Checks for all winning possiblities
   for (var i = 0; i < 3; i++) {
     winCheck[0] += this.spotCheck(spot[0], i); //row check
     winCheck[1] += this.spotCheck(i, spot[1]); //col check
+    winCheck[2] += this.spotCheck(i, i); //diagonal one check
+    winCheck[3] += this.spotCheck(i, 2-i); //diagonal two check
   }
-  winCheck[2] = this.spotCheck(0, 0) + this.spotCheck(1, 1) + this.spotCheck(2, 2); //check diagonal one
-  winCheck[3] = this.spotCheck(0, 2) + this.spotCheck(1, 1) + this.spotCheck(2, 0); //check diagonal two
 
   if (winCheck.includes(3)) return true;
   if (this.turnNumber === 9) return NaN;
@@ -44,11 +43,11 @@ Game.prototype.checkWin = function(spot) {
 
 
  //UI Logic
+ var freshGame = "";
 $(document).ready(function(){
-  var freshGame = "";
   var winner = "";
 
-  $("button#start").click(function(){
+  $("div#start").click(function(){
 
     $("div#startGame").hide();
     $("div#gameBoard").show();
@@ -56,23 +55,29 @@ $(document).ready(function(){
 
   });
 
-  $(".col-sm-4").click(function(){
+  $(".col-sm-4").click(function(){   // Click function to set id of current turn
     var spot = this.id.split("");
 
-    if ( !(freshGame.board[spot[0]][spot[1]]) ) {
+//checks if something is on current spot and if someone has won //
+
+    if ( !(freshGame.board[spot[0]][spot[1]]) && (!winner)) {
       $(this).children().attr("src", "img/" + freshGame.currentPlayer + ".png");
       winner = freshGame.playerMove(spot);
     } else {
       alert("Cheaters never win."); // "unless they "R" smart"
     }
 
-    if (winner !== winner) {
+    if (winner !== winner) {   // Conpletes action on win
       alert("Cat always wins!");
     } else if (winner && (freshGame.currentPlayer === "alien")){
       alert ("Congratulations, you've conquered the Earth and ruined Humanity");
     } else if (winner && (freshGame.currentPlayer === "human")){
       alert("Thank you Mario but, our princess is in another Galaxy...........");
     }
+
+  });
+  $("#cheatR").click(function(){
+    freshGame.cheatR[freshGame.currentPlayer]++;
   });
 
 });
